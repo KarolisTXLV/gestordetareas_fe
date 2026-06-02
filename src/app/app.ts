9858d10836090264767a1b88b4f2d2554cab40ef
Karolis Tamosaitis Kembre <ktamosaxlv@gmail.com>
2026-06-02 13:16:03 +0200

    fix: arreglo analiticas de vercel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from "./components/toast/toast";
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics';
+
+inject();
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent,],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
